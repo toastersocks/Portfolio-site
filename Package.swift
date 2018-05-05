e17135587cb001ca11de039b3fde6378c033084d
James Pamplona <pamplonajames@gmail.com>
2018-05-05 09:56:46 -0700

    Update Vapor version

--- Package.swift
+++ Package.swift
@@ -5,12 +5,12 @@ let package = Package(
     name: "Portfolio",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.1"),
 
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2.2"),
         
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2")
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2.2")
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Leaf"]),
